c632aae0fcbd3b5d66067abd2cd6a90632b165c3
ManonOgr <72557754+ManonOgr@users.noreply.github.com>
2023-07-19 17:39:08 +0200

    change css

--- admin/src/components/AdminDashboard.vue
+++ admin/src/components/AdminDashboard.vue
@@ -1,6 +1,6 @@
 <script></script>
 <template>
-  <div>
+  <div class="container">
     <h1>Dashboard admin</h1>
     <table>
       <thead>
@@ -23,9 +23,52 @@
       </tbody>
     </table>
     <div>
-      <button class="btn">
-        Ajouter une paire 
-      </button>
+      <button class="btn add">Ajouter une paire</button>
     </div>
   </div>
 </template>
+
+<style>
+::before,
+::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  height: 100vh;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+thead tr {
+  background-color: #000;
+  color: #fff;
+  text-align: left;
+}
+
+th,
+td {
+  padding: 15px 20px;
+}
+
+tbody tr,
+td,
+th {
+  border: 2px solid #000;
+}
+
+tbody tr:nth-child(event) {
+  background-color: #f3f3f3;
+}
+
+.add {
+  margin-top: 30px;
+}
+</style>
--- admin/src/components/AdminForm.vue
+++ admin/src/components/AdminForm.vue
@@ -2,49 +2,106 @@
 </script>
 
 <template>
-  <section>
-    
-      <form class="form-horizontal" method="post">
-
-        <h3 class="title">Login</h3>
-        <div class="form-group">
-          <input name="email" type="email" class="form-control" placeholder="E-mail" />
-        </div>
-        <div class="form-group">
-
-          <input name="password" type="password" class="form-control" placeholder="Password"/>
-        </div>
-        <button type="submit" class="btn btn-primary">Login</button>
-      </form>
-
-  </section>
+  <div class="login-box">
+    <h2>Login</h2>
+    <form>
+      <div class="user-box">
+        <input
+          type="email"
+          name="email"
+          required=""
+          placeholder="email"
+        />
+        <label></label>
+      </div>
+      <div class="user-box">
+        <input
+          type="password"
+          name="password"
+          required=""
+          placeholder="password"
+        />
+        <label></label>
+      </div>
+      <button type="submit">
+        Login
+      </button>
+    </form>
+  </div>
 </template>
 
-<style scoped>
-
-section{
-  display: flex;
-  align-items: center;
-  justify-content: center;
+<style>
+html {
+  height: 100%;
+}
+body {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  background: linear-gradient(#007f5f, #6096ba);
 }
 
-form{
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+.login-box {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 400px;
+  padding: 40px;
+  transform: translate(-50%, -50%);
+  background: rgba(0, 0, 0, 0.5);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
   border-radius: 10px;
-  width: 0%;
-  background-color: aqua;
 }
 
-.form-group{
-  margin-top: 20px;
+.login-box h2 {
+  margin: 0 0 30px;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+}
+
+.login-box .user-box {
+  position: relative;
+}
+
+.login-box .user-box input {
+  width: 100%;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 1px solid #fff;
+  outline: none;
+  background: transparent;
+}
+.login-box .user-box label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  pointer-events: none;
+  transition: 0.5s;
+}
+
+.login-box .user-box input:focus ~ label,
+.login-box .user-box input:valid ~ label {
+  top: -20px;
+  left: 0;
+  color: #03e9f4;
+  font-size: 12px;
 }
 
 button{
-  margin-bottom: 30px;
-  margin-top: 20px;
+  background-color: #CDCBCB;
+  border: none;
+  border-radius: 20px;
+  margin: 0 0 30px;
+  width: 150px;
+  height: 30px;
 }
 
 </style>
