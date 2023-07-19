e352740fca212b2504fd40542a53532a7237c078
ManonOgr <72557754+ManonOgr@users.noreply.github.com>
2023-07-19 16:33:19 +0200

    add convertView and routes

--- admin/src/routers/index.js
+++ admin/src/routers/index.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import CurrenciesView from '@/views/CurrenciesView.vue'
 import AdminView from '@/views/AdminView.vue'
+import ConvertView from '@/views/ConvertView.vue'
 
 
 const router = createRouter({
@@ -24,6 +25,12 @@ const router = createRouter({
       name: "admin",
       component: AdminView
     },
+    {
+      path: "/convert",
+      name: "convert",
+      component: ConvertView
+    },
+
     
   ]
 })
