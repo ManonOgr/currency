9a697322c54ab4ef8babac564e7ba1ed854418cf
ManonOgr <72557754+ManonOgr@users.noreply.github.com>
2023-07-19 17:09:18 +0200

    change view

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,70 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <div>
-    <nav>
-      <RouterLink to="/currency">Currency's list</RouterLink>
-      <RouterLink to="/convert">Convert</RouterLink>
-      <RouterLink to="/home">Login</RouterLink>
-
-    </nav>
-  </div>
-      
-  </header>
+  <header></header>
 
   <RouterView />
 </template>
 
 <style scoped>
-header, nav{
- display: flex;
- flex-direction: row;
- justify-content: center;
- align-items: center;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
