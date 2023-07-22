7e4d580bfec3edc1b5a93e02ee5b1a97f2937f44
ManonOgr <72557754+ManonOgr@users.noreply.github.com>
2023-07-22 10:24:27 +0200

    add btn url list devices

--- admin/src/components/NavBarDashboard.vue
+++ admin/src/components/NavBarDashboard.vue
@@ -3,5 +3,7 @@
         <v-btn variant="tonal">
             Disconnect
           </v-btn>
+          <v-btn variant="tonal"> Ajout </v-btn>
+          <v-btn variant="tonal"> <a href="/currency">Liste des devices</a></v-btn>
     </nav>
 </template>
