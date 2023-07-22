95b3f6f4b90283696f9f709ec1e1403c0bce6e9f
ManonOgr <72557754+ManonOgr@users.noreply.github.com>
2023-07-22 19:06:41 +0200

    create view addpairs

--- admin/src/routers/index.js
+++ admin/src/routers/index.js
@@ -1,15 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
+import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import CurrenciesView from '@/views/CurrenciesView.vue'
 import AdminView from '@/views/AdminView.vue'
+import EditPairs from '@/views/EditPairsView.vue'
 import ConvertView from '@/views/ConvertView.vue'
+import AddPairs from '@/views/AddPairsView.vue'
 
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
     {
       path: '/admin/login',
       name: 'login',
@@ -29,6 +36,18 @@ const router = createRouter({
       path: "/convert",
       name: "convert",
       component: ConvertView
+    },
+    {
+      path: "/edit/:id",
+      name: "edit",
+      component: EditPairs,
+      props: true
+    },
+    {
+      path: "/add/:id",
+      name: "add",
+      component: AddPairs,
+      props: true
     },
 
     
---
+++ admin/src/views/AddPairsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <form>
+    <div class="row col-lg-12">
+      <h1>Ajouter une paire</h1>
+      <div class="mb-3 col-lg-3">
+        <label for="rate">Nom de la paire</label>
+        <v-text-field id="input" placeholder="CAD / AUD "></v-text-field>
+      </div>
+      <div class="mb-3 col-lg-3">
+        <label for="rate">Taux de change</label>
+        <v-text-field id="input" placeholder="0"></v-text-field>
+      </div>
+      <div class="col-lg-3 m-auto">
+        <button
+          type="submit"
+          class="btn btn-primary"
+        >
+          Ajouter
+        </button>
+      </div>
+    </div>
+  </form>
+</template>
